e903156b68ef34b23f95132ecffae8f55ca5a797
Sarina <sarinahamedani3@gmail.com>
2021-07-27 12:25:25 +0430

    Add test for requesting a course twice and one that has already been passed

--- src/test/java/ir/proprog/enrollassist/domain/EnrollmentListTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/EnrollmentListTest.java
@@ -28,4 +28,40 @@ public class EnrollmentListTest {
                 .isNotNull()
                 .hasSize(1);
     }
+
+    @Test
+    void Enrollment_list_course_already_passed_rule_fails() {
+        Student bebe = mock(Student.class);
+        Course math1 = new Course("1", "MATH1", 3);
+        Course phys1 = new Course("2", "PHYS1", 3);
+        Course prog = new Course("3", "PROG", 4);
+        Course math2 = new Course("5", "MATH2", 3).withPre(math1);
+        Section math1_1 = new Section(math1, "01");
+        Section phys1_1 = new Section(phys1, "01");
+        Section math2_1 = new Section(math2, "01");
+        Section prog_1 = new Section(prog, "01");
+        when(bebe.hasPassed(math1)).thenReturn(true);
+        EnrollmentList list = new EnrollmentList("bebe's list", bebe);
+        list.addSections(math1_1, phys1_1, math2_1, prog_1);
+//        System.out.println(list.checkEnrollmentRules());
+        assertThat(list.checkEnrollmentRules())
+                .isNotNull()
+                .hasSize(1);
+    }
+
+    @Test
+    void Enrollment_list_course_requested_twice_rule_fails() {
+        Student bebe = mock(Student.class);
+        Course math1 = new Course("1", "MATH1", 3);
+        Course phys1 = new Course("2", "PHYS1", 3);
+        Course prog = new Course("3", "PROG", 4);
+        Section math1_1 = new Section(math1, "01");
+        Section phys1_1 = new Section(phys1, "01");
+        Section prog_1 = new Section(prog, "01");
+        EnrollmentList list = new EnrollmentList("bebe's list", bebe);
+        list.addSections(math1_1, phys1_1, prog_1, math1_1);
+        assertThat(list.checkEnrollmentRules())
+                .isNotNull()
+                .hasSize(1);
+    }
 }
