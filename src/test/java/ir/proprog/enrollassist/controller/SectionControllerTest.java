89e359313077b4cc3f3ed8dd537334cf0d21abdf
Sarina <sarinahamedani3@gmail.com>
2021-07-28 08:14:34 +0430

    Add tests for section controller

--- src/test/java/ir/proprog/enrollassist/controller/SectionControllerTest.java
+++ src/test/java/ir/proprog/enrollassist/controller/SectionControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
@@ -47,5 +48,48 @@ public class SectionControllerTest {
                 .andExpect(jsonPath("$[0].courseTitle", is("C1")))
                 .andExpect(jsonPath("$[0].courseNumber", is("1")))
                 .andExpect(jsonPath("$[0].sectionNo", is("01")));
+    }
+
+    @Test
+    public void One_section_is_returned_correctly() throws Exception {
+        Section section = new Section(new Course("1", "C1", 3), "01");
+
+        given(sectionRepository.findById(1L)).willReturn(java.util.Optional.of(section));
+
+        mvc.perform(get("/sections/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.courseTitle", is("C1")))
+                .andExpect(jsonPath("$.courseNumber", is("1")))
+                .andExpect(jsonPath("$.sectionNo", is("01")));
+    }
+
+    @Test
+    public void All_section_demands_are_returned_correctly() throws Exception {
+        Section s1 = new Section(new Course("1", "C1", 3), "01");
+        SectionView c1 = new SectionView(s1);
+        SectionDemandView sd_1 = new SectionDemandView(c1, 50);
+        Section s2 = new Section(new Course("2", "C2", 3), "01");
+        SectionView c2 = new SectionView(s2);
+        SectionDemandView sd_2 = new SectionDemandView(c2, 72);
+        Section s3 = new Section(new Course("3", "C3", 3), "01");
+        SectionView c3 = new SectionView(s3);
+        SectionDemandView sd_3 = new SectionDemandView(c3, 25);
+
+        List<SectionDemandView> demands = List.of(sd_1, sd_2, sd_3);
+
+        given(enrollmentListRepository.findDemandForAllSections()).willReturn(demands);
+
+        given(sectionRepository.findById(sd_1.getSectionId())).willReturn(java.util.Optional.of(s1));
+        given(sectionRepository.findById(sd_2.getSectionId())).willReturn(java.util.Optional.of(s2));
+        given(sectionRepository.findById(sd_3.getSectionId())).willReturn(java.util.Optional.of(s3));
+
+        mvc.perform(get("/sections/demands")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(demands.size())))
+                .andExpect(jsonPath("$[0].demand", is(50)))
+                .andExpect(jsonPath("$[1].demand", is(72)))
+                .andExpect(jsonPath("$[2].demand", is(25)));
     }
 }
