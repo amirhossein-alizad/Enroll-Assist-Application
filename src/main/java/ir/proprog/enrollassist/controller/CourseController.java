280a9f1c897e1c223abdef499f86f45d8d0916d0
amir.alizad <aalizad79@gmail.com>
2021-08-02 12:15:13 +0430

    Add Add_new_course function

--- src/main/java/ir/proprog/enrollassist/controller/CourseController.java
+++ src/main/java/ir/proprog/enrollassist/controller/CourseController.java
@@ -1,12 +1,15 @@
 package ir.proprog.enrollassist.controller;
 
+import ir.proprog.enrollassist.domain.Course;
 import ir.proprog.enrollassist.domain.EnrollmentList;
 import ir.proprog.enrollassist.domain.Section;
+import ir.proprog.enrollassist.domain.Student;
 import ir.proprog.enrollassist.repository.CourseRepository;
 import ir.proprog.enrollassist.repository.EnrollmentListRepository;
 import ir.proprog.enrollassist.repository.SectionRepository;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -19,5 +22,11 @@ public class CourseController {
 
     public CourseController(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
+    }
+
+    @PostMapping(value = "/addNew", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public void Add_new_course(@RequestBody Course course){
+        System.out.println(course.getTitle());
+        courseRepository.save(course);
     }
 }
