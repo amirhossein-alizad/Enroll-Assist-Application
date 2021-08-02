0744162bdbce33de2591ecf8a98b0e4c5efcce94
amir.alizad <aalizad79@gmail.com>
2021-08-02 15:00:29 +0430

    Add another constructor for CourseView

--- src/main/java/ir/proprog/enrollassist/controller/CourseView.java
+++ src/main/java/ir/proprog/enrollassist/controller/CourseView.java
@@ -1,12 +1,16 @@
 package ir.proprog.enrollassist.controller;
 
 import ir.proprog.enrollassist.domain.Course;
+import lombok.Getter;
+import java.util.*;
 
+@Getter
 public class CourseView {
     private Long courseId;
     private String courseNumber;
     private String courseTitle;
     private int courseCredits;
+    private Set<Long> prerequisites = new HashSet<>();
 
     public CourseView() {
     }
@@ -16,5 +20,16 @@ public class CourseView {
         this.courseNumber = course.getCourseNumber();
         this.courseTitle = course.getTitle();
         this.courseCredits = course.getCredits();
+        if(!course.getPrerequisites().isEmpty())
+            for(Course c : course.getPrerequisites())
+                prerequisites.add(c.getId());
+    }
+
+    public CourseView(Course course, Set<Long> prerequisites) {
+        this.courseId = course.getId();
+        this.courseNumber = course.getCourseNumber();
+        this.courseTitle = course.getTitle();
+        this.courseCredits = course.getCredits();
+        this.prerequisites = prerequisites;
     }
 }
