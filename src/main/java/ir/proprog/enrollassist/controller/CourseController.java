72f79bda18a4809639ae180b7323541696b6ae48
amir.alizad <aalizad79@gmail.com>
2021-08-02 15:41:38 +0430

    Fix Add_new_course function

--- src/main/java/ir/proprog/enrollassist/controller/CourseController.java
+++ src/main/java/ir/proprog/enrollassist/controller/CourseController.java
@@ -1,18 +1,14 @@
 package ir.proprog.enrollassist.controller;
 
 import ir.proprog.enrollassist.domain.Course;
-import ir.proprog.enrollassist.domain.EnrollmentList;
-import ir.proprog.enrollassist.domain.Section;
 import ir.proprog.enrollassist.domain.Student;
 import ir.proprog.enrollassist.repository.CourseRepository;
-import ir.proprog.enrollassist.repository.EnrollmentListRepository;
-import ir.proprog.enrollassist.repository.SectionRepository;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 @RestController
@@ -30,8 +26,14 @@ public class CourseController {
     }
 
     @PostMapping(value = "/addNewCourse", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public CourseView Add_new_course(@RequestBody Course course){
-        courseRepository.save(course);
-        return new CourseView(course);
+    public CourseView Add_new_course(@RequestBody CourseView courseView){
+        Course newCourse = new Course(courseView.getCourseNumber(), courseView.getCourseTitle(), courseView.getCourseCredits());
+        for(Long L : courseView.getPrerequisites()){
+            Course prerequisite = courseRepository.findById(L)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Prerequisite course not found"));
+            newCourse.withPre(prerequisite);
+        }
+        courseRepository.save(newCourse);
+        return new CourseView(newCourse);
     }
 }
