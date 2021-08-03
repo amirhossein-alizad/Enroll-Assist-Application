97726c7f69561e1f2e85c3ab68607709a245a773
amir.alizad <aalizad79@gmail.com>
2021-08-03 14:11:15 +0430

    Add CourseNumber validation

--- src/main/java/ir/proprog/enrollassist/controller/CourseController.java
+++ src/main/java/ir/proprog/enrollassist/controller/CourseController.java
@@ -1,13 +1,14 @@
 package ir.proprog.enrollassist.controller;
 
-import ir.proprog.enrollassist.domain.Course;
-import ir.proprog.enrollassist.domain.Student;
+import ir.proprog.enrollassist.domain.*;
 import ir.proprog.enrollassist.repository.CourseRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -27,7 +28,9 @@ public class CourseController {
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public CourseView addNewCourse(@RequestBody CourseView courseView){
+        List<CourseRuleViolation> courseRuleViolations = new ArrayList<>();
         Course newCourse = new Course(courseView.getCourseNumber(), courseView.getCourseTitle(), courseView.getCourseCredits());
+        courseRuleViolations.addAll(this.ValidateCourseNumber(newCourse.getCourseNumber()));
         for(Long L : courseView.getPrerequisites()){
             Course prerequisite = courseRepository.findById(L)
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Prerequisite course not found"));
@@ -35,6 +38,22 @@ public class CourseController {
         }
         courseRepository.save(newCourse);
         return new CourseView(newCourse);
+    }
+
+    private List<CourseRuleViolation> ValidateCourseNumber(String courseNumber){
+        List<CourseRuleViolation> courseRuleViolations = new ArrayList<>();
+        if(courseNumber.equals(""))
+            courseRuleViolations.add(new CourseNumberEmpty());
+        else{
+            try {
+                Integer.parseInt(courseNumber);
+                if(courseRepository.FindCourseByCourseNumber(courseNumber).isPresent())
+                    courseRuleViolations.add(new CourseNumberExists());
+            } catch (NumberFormatException numberFormatException) {
+                courseRuleViolations.add(new WrongCourseNumberFormat());
+            }
+        }
+        return courseRuleViolations;
     }
 
     @GetMapping("/{id}")
