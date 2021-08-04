6a5f534f271b7d0d393074377065361db72f9286
SarinaHmd <sarinahamedani3@gmail.com>
2021-08-04 11:08:49 +0430

    Add query for finding all students enrollment lists

--- src/main/java/ir/proprog/enrollassist/repository/StudentRepository.java
+++ src/main/java/ir/proprog/enrollassist/repository/StudentRepository.java
@@ -1,10 +1,16 @@
 package ir.proprog.enrollassist.repository;
 
+import ir.proprog.enrollassist.controller.SectionDemandView;
 import ir.proprog.enrollassist.domain.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
     Optional<Student> findByStudentNumber(String studentNumber);
+
+    @Query(value = "select new ir.proprog.enrollassist.controller.EnrollmentListView(list) from EnrollmentList list join Student student where student.studentNumber=?1 and list.owner.studentNumber=?1")
+    List<SectionDemandView> findAllListsForStudent(String studentNo);
 }
