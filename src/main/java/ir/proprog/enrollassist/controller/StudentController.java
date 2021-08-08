e54caf2f1c9488d93500cb3ea93872578953cb35
awwmiir <amir.alizad@ut.ac.ir>
2021-08-08 19:19:51 +0430

    Add takeableSections method

--- src/main/java/ir/proprog/enrollassist/controller/StudentController.java
+++ src/main/java/ir/proprog/enrollassist/controller/StudentController.java
@@ -1,14 +1,18 @@
 package ir.proprog.enrollassist.controller;
 
+import ir.proprog.enrollassist.domain.Course;
 import ir.proprog.enrollassist.domain.EnrollmentList;
 import ir.proprog.enrollassist.domain.Student;
+import ir.proprog.enrollassist.repository.CourseRepository;
 import ir.proprog.enrollassist.repository.StudentRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -17,9 +21,11 @@ import java.util.stream.StreamSupport;
 @RequestMapping("/student")
 public class StudentController {
     private StudentRepository studentRepository;
+    private CourseRepository courseRepository;
 
-    public StudentController(StudentRepository studentRepository) {
+    public StudentController(StudentRepository studentRepository, CourseRepository courseRepository) {
         this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
     }
 
     @GetMapping("/all")
@@ -37,7 +43,7 @@ public class StudentController {
     public StudentView addStudent(@RequestBody StudentView studentView) {
         Optional<Student> student = this.studentRepository.findByStudentNumber(studentView.getStudentNo());
         if (student.isPresent())
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "This section is already exists.");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "This student already exists.");
         try {
             Student newStudent = new Student(studentView.getStudentNo(), studentView.getName());
             this.studentRepository.save(newStudent);
@@ -45,6 +51,21 @@ public class StudentController {
         }catch (IllegalArgumentException illegalArgumentException) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Student number or student name is not valid.");
         }
+    }
+
+    @GetMapping("/takeableSections/{studentNumber}")
+    public Iterable<CourseView> takeableSections(@PathVariable String studentNumber){
+        List<CourseView> takeable  = new ArrayList<>();
+        List<Course> notPassed = (List<Course>)courseRepository.findAll();
+        Optional<Student> student = this.studentRepository.findByStudentNumber(studentNumber);
+        if(student.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found.");
+        Student std = student.get();
+        notPassed.removeAll(std.getPassedCourses());
+        for(Course c : notPassed)
+            if(c.canBeTakenBy(std).isEmpty())
+                takeable.add(new CourseView(c));
+        return takeable;
     }
 
 }
