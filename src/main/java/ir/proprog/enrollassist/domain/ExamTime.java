ea3d3a447a00e3bec8430bc33cdf673d379d4268
Sarina <sarinahamedani3@gmail.com>
2021-08-09 19:16:07 +0430

    Modify date type and format for exam time

--- src/main/java/ir/proprog/enrollassist/domain/ExamTime.java
+++ src/main/java/ir/proprog/enrollassist/domain/ExamTime.java
@@ -10,6 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -17,35 +21,35 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExamTime {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Date start;
-    private Date end;
+    private LocalDateTime start;
+    private LocalDateTime end;
 
     public List<String> validate() {
         List<String> errors = new ArrayList<>();
         if (this.start.compareTo(this.end) >= 0)
             errors.add("Exam start should be before its end.");
-        Instant instant1 = this.start.toInstant()
-                .truncatedTo(ChronoUnit.DAYS);
-        Instant instant2 = this.end.toInstant()
-                .truncatedTo(ChronoUnit.DAYS);
-        if (!instant1.equals(instant2))
+
+        if (!this.start.toLocalDate().equals(this.end.toLocalDate()))
             errors.add("Exam cannot take more than one day.");
+
         return errors;
     }
 
     public ExamTime(String start, String end) throws Exception {
-        SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        Calendar calendar = Calendar.getInstance();
         try {
-            this.start = dataFormat.parse(start);
-            this.end = dataFormat.parse(end);
+            this.start = LocalDateTime.parse(start, dateFormat);
+            this.end = LocalDateTime.parse(end, dateFormat);
         } catch (Exception e) {
             System.out.println(e);
-            throw new Exception("Dates must be of the format yyyy-MM-dd'T'hh:mm:ss");
+            throw new Exception("Dates must be of the format yyyy-MM-dd HH:mm");
         }
     }
 
