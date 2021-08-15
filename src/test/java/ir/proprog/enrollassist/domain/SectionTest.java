214aa21404ca372a826d562bcebc9ae45d72b5a1
awwmiir <amir.alizad@ut.ac.ir>
2021-08-15 18:59:53 +0430

    Add test for scheduleToString

--- src/test/java/ir/proprog/enrollassist/domain/SectionTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/SectionTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
@@ -23,4 +25,15 @@ public class SectionTest {
         assertEquals(exception, "Section number must be number");
     }
 
+    @Test
+    public void scheduleToString_method_works_correctly() throws Exception {
+        Course math1 = new Course("1111111", "MATH1", 3);
+        Section section = new Section(math1, "01");
+        section.setPresentationSchedule(Set.of( new PresentationSchedule("Sunday", "10:00", "12:00"), new PresentationSchedule("Wednesday", "10:00", "12:00")));
+        assertThat(section.scheduleToString())
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(2)
+                .containsExactlyInAnyOrder("Sunday 10:00 - 12:00", "Wednesday 10:00 - 12:00");
+    }
 }
