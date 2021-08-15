6a63183595ebde789e2baa34a8d899a8c96a9487
awwmiir <amir.alizad@ut.ac.ir>
2021-08-15 21:02:20 +0430

    Change PresentationSchedule field types

--- src/main/java/ir/proprog/enrollassist/domain/PresentationSchedule.java
+++ src/main/java/ir/proprog/enrollassist/domain/PresentationSchedule.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.text.DateFormatSymbols;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -17,39 +18,35 @@ import java.util.List;
 @Embeddable
 public class PresentationSchedule {
     private String dayOfWeek;
-    private Date startTime = new Date();
-    private Date endTime = new Date();
-    private String stringFormat;
+    private String startTime;
+    private String endTime;
 
     public PresentationSchedule(String dayOfWeek, String start, String end) throws ExceptionList {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = start;
+        this.endTime = end;
         ExceptionList exceptionList = new ExceptionList();
         try {
-            this.validateDayOfWeek(dayOfWeek);
-            this.dayOfWeek = dayOfWeek;
+           this.validateFields();
         } catch (Exception exception) {
             exceptionList.addNewException(exception);
         }
-        try {
-            validateTime(start, end);
-        }catch (Exception e) {
-            exceptionList.addNewException(e);
-        }
         if (exceptionList.hasException())
             throw exceptionList;
-        stringFormat = dayOfWeek + " " + start + " - " + end;
+
 
     }
 
     private void validateTime(String start, String end) throws Exception {
+        SimpleDateFormat dataFormat = new SimpleDateFormat("HH:mm");
         try {
-            SimpleDateFormat dataFormat = new SimpleDateFormat("HH:mm");
-            this.startTime = dataFormat.parse(start);
-            this.endTime = dataFormat.parse(end);
+            dataFormat.parse(start);
+            dataFormat.parse(end);
         }
         catch (Exception exception) {
             throw new Exception("Time format is not valid");
         }
-        if (this.endTime.before(this.startTime))
+        if (dataFormat.parse(start).before(dataFormat.parse(end)))
             throw new Exception("End time can not be before start time.");
     }
 
@@ -62,13 +59,40 @@ public class PresentationSchedule {
     }
 
     public boolean hasConflict(PresentationSchedule otherPresentationSchedule) {
+        SimpleDateFormat dataFormat = new SimpleDateFormat("HH:mm");
+
         if (!this.dayOfWeek.equals(otherPresentationSchedule.dayOfWeek))
             return false;
-        else if (otherPresentationSchedule.startTime.after(this.endTime) || otherPresentationSchedule.startTime.equals(this.endTime))
-            return false;
-        else return !otherPresentationSchedule.endTime.before(this.startTime) && !otherPresentationSchedule.endTime.equals(this.startTime);
+        else {
+            try {
+                if (dataFormat.parse(otherPresentationSchedule.startTime).after(dataFormat.parse(this.endTime))
+                        || dataFormat.parse(otherPresentationSchedule.startTime).equals(dataFormat.parse(this.endTime)))
+                    return false;
+                else return !dataFormat.parse(otherPresentationSchedule.endTime).before(dataFormat.parse(this.startTime))
+                        && !dataFormat.parse(otherPresentationSchedule.endTime).equals(dataFormat.parse(this.startTime));
+            } catch (ParseException ignored) {
+
+            }
+        }
+        return false;
     }
 
+
+    public void validateFields() throws ExceptionList {
+        ExceptionList exceptionList = new ExceptionList();
+        try {
+            this.validateDayOfWeek(dayOfWeek);
+        } catch (Exception exception) {
+            exceptionList.addNewException(exception);
+        }
+        try {
+            validateTime(startTime, endTime);
+        }catch (Exception e) {
+            exceptionList.addNewException(e);
+        }
+        if (exceptionList.hasException())
+            throw exceptionList;
+    }
     @Override
-    public String toString() { return stringFormat; }
+    public String toString() { return dayOfWeek + " " + startTime + " - " + endTime; }
 }
