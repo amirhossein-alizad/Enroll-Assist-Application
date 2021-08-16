ab8e195b99a7578997b05f5e99a2f6662776cf6f
awwmiir <amir.alizad@ut.ac.ir>
2021-08-16 18:33:36 +0430

    Add FacultyRepository

---
+++ src/main/java/ir/proprog/enrollassist/repository/FacultyRepository.java
@@ -0,0 +1,7 @@
+package ir.proprog.enrollassist.repository;
+
+import ir.proprog.enrollassist.domain.Faculty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FacultyRepository extends CrudRepository<Faculty, Long> {
+}
