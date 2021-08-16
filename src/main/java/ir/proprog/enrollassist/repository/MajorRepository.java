2220025658514e797829af375536634fead500c9
awwmiir <amir.alizad@ut.ac.ir>
2021-08-16 18:03:12 +0430

    Add MajorRepository

---
+++ src/main/java/ir/proprog/enrollassist/repository/MajorRepository.java
@@ -0,0 +1,7 @@
+package ir.proprog.enrollassist.repository;
+
+import ir.proprog.enrollassist.domain.Major;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MajorRepository extends CrudRepository<Major, Long> {
+}
