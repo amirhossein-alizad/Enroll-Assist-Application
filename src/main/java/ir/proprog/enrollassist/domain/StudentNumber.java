6016923468915b034ac4669999b7190a07607523
awwmiir <amir.alizad@ut.ac.ir>
2021-08-17 12:51:51 +0430

    Rename fields in StudentNumber class

--- src/main/java/ir/proprog/enrollassist/domain/StudentNumber.java
+++ src/main/java/ir/proprog/enrollassist/domain/StudentNumber.java
@@ -1,28 +1,31 @@
 package ir.proprog.enrollassist.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import javax.persistence.Embeddable;
 import java.util.Objects;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class StudentNumber {
-    private final String studentNumber;
+    private String number;
 
     public StudentNumber(String studentNumber) {
         if(studentNumber.equals(""))
             throw new IllegalArgumentException("Student number can not be empty.");
-        this.studentNumber = studentNumber;
+        this.number = studentNumber;
     }
 
     @Override
-    public int hashCode() { return Objects.hash(studentNumber); }
+    public int hashCode() { return Objects.hash(number); }
 
     @Override
     public boolean equals(Object o){
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         StudentNumber stdNo = (StudentNumber) o;
-        return studentNumber.equals(stdNo.studentNumber);
+        return number.equals(stdNo.number);
     }
 }
