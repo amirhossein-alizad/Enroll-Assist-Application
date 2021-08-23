d612fcd14c905efaef2c7e4300ac24b67916c106
Sarina <sarinahamedani3@gmail.com>
2021-08-23 16:40:51 +0430

    Add faculty view

---
+++ src/main/java/ir/proprog/enrollassist/controller/FacultyView.java
@@ -0,0 +1,17 @@
+package ir.proprog.enrollassist.controller;
+
+import ir.proprog.enrollassist.domain.Faculty;
+import ir.proprog.enrollassist.domain.Major;
+
+public class FacultyView {
+    private Long facultyId;
+    private String facultyName;
+
+    public FacultyView() {
+    }
+
+    public FacultyView(Faculty faculty) {
+        this.facultyId = faculty.getId();
+        this.facultyName = faculty.getFacultyName();
+    }
+}
