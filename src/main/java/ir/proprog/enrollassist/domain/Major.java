b91268fbf67f5e4b476c6029397eff9ecd51034c
Sarina <sarinahamedani3@gmail.com>
2021-08-23 17:39:56 +0430

    Add tests for adding new major

--- src/main/java/ir/proprog/enrollassist/domain/Major.java
+++ src/main/java/ir/proprog/enrollassist/domain/Major.java
@@ -27,7 +27,7 @@ public class Major {
     public Major(String majorNumber, String majorName) throws ExceptionList {
         ExceptionList exceptionList = new ExceptionList();
         if(majorName.equals(""))
-            exceptionList.addNewException(new Exception("Major name can not be empty."));
+            exceptionList.addNewException(new Exception("Major name can not be empty"));
         if(majorNumber.equals(""))
             exceptionList.addNewException(new Exception("Major number can not be empty"));
         if(exceptionList.hasException())
--- src/test/java/ir/proprog/enrollassist/controller/MajorControllerTest.java
+++ src/test/java/ir/proprog/enrollassist/controller/MajorControllerTest.java
@@ -1,7 +1,9 @@
 package ir.proprog.enrollassist.controller;
 
+import ir.proprog.enrollassist.domain.Faculty;
 import ir.proprog.enrollassist.domain.Major;
 import ir.proprog.enrollassist.repository.*;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,8 +15,11 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,5 +46,74 @@ public class MajorControllerTest {
                 .andExpect(jsonPath("$[0].majorNumber", is("8101")))
                 .andExpect(jsonPath("$[1].majorName", is("CHEM")))
                 .andExpect(jsonPath("$[1].majorNumber", is("8102")));
+    }
+
+    @Test
+    public void Valid_major_is_added_correctly() throws Exception{
+        Faculty faculty = mock(Faculty.class);
+        JSONObject request = new JSONObject();
+        request.put("majorName", "CHEM");
+        request.put("majorNumber", "8102");
+        given(facultyRepository.findById(1L)).willReturn(java.util.Optional.ofNullable(faculty));
+        mvc.perform(post("/majors/1")
+                .content(request.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.majorName", is("CHEM")))
+                .andExpect(jsonPath("$.majorNumber", is("8102")));
+    }
+
+    @Test
+    public void Major_cannot_be_added_if_name_already_exists() throws Exception{
+        Faculty faculty = mock(Faculty.class);
+        Major major = mock(Major.class);
+        JSONObject request = new JSONObject();
+        request.put("majorName", "EE");
+        request.put("majorNumber", "8101");
+
+        JSONObject response = new JSONObject();
+        response.put("1", "Major with name EE exists.");
+
+        given(facultyRepository.findById(1L)).willReturn(java.util.Optional.ofNullable(faculty));
+        given(majorRepository.findByMajorName("EE")).willReturn(java.util.Optional.ofNullable(major));
+        mvc.perform(post("/majors/1")
+                .content(request.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(mvcResult -> assertEquals(mvcResult.getResponse().getErrorMessage(), response.toString()));
+    }
+
+    @Test
+    public void Major_cannot_be_added_if_faculty_does_not_exists() throws Exception{
+        Major major = mock(Major.class);
+        JSONObject request = new JSONObject();
+        request.put("majorName", "EE");
+        request.put("majorNumber", "8101");
+        given(majorRepository.findByMajorName("EE")).willReturn(java.util.Optional.ofNullable(major));
+        mvc.perform(post("/majors/1")
+                .content(request.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(mvcResult -> assertEquals(mvcResult.getResponse().getErrorMessage(), "Faculty not found"));
+    }
+
+    @Test
+    public void Major_name_and_number_cannot_be_empty() throws Exception{
+        Faculty faculty = mock(Faculty.class);
+        JSONObject request = new JSONObject();
+        request.put("majorName", "");
+        request.put("majorNumber", "");
+
+        JSONObject response = new JSONObject();
+        response.put("1", "Major name can not be empty");
+        response.put("2", "Major number can not be empty");
+
+        given(facultyRepository.findById(1L)).willReturn(java.util.Optional.ofNullable(faculty));
+
+        mvc.perform(post("/majors/1")
+                .content(request.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(mvcResult -> assertEquals(mvcResult.getResponse().getErrorMessage(), response.toString()));
     }
 }
