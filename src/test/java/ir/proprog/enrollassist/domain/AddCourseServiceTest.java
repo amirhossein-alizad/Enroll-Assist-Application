5af25126aadc9eef3c15451f2c4099442706f49c
Sarina <sarinahamedani3@gmail.com>
2021-08-25 20:03:01 +0430

    Modify course service tests

--- src/test/java/ir/proprog/enrollassist/domain/AddCourseServiceTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/AddCourseServiceTest.java
@@ -4,37 +4,36 @@ import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.controller.CourseMajorView;
 import ir.proprog.enrollassist.repository.CourseRepository;
 import ir.proprog.enrollassist.repository.MajorRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 public class AddCourseServiceTest {
     private AddCourseService addCourseService;
-    Faculty faculty;
-    @Autowired
+    private Faculty faculty;
+    @MockBean
     private CourseRepository courseRepository;
-    @Autowired
+    @MockBean
     private MajorRepository majorRepository;
 
     @BeforeEach
     public void setUp() throws ExceptionList {
         Course course1 = new Course("1100112", "DS", 3);
         Course course2 = new Course("1100113", "DA", 3);
-        courseRepository.save(course2);
-        courseRepository.save(course1);
+        given(courseRepository.findAll()).willReturn(List.of(course1, course2));
         Major ce = new Major("1", "CE");
         Major ee = new Major("2", "EE");
         ce.addCourse(course1);
         ee.addCourse(course2);
-        majorRepository.save(ee);
-        majorRepository.save(ce);
+        given(majorRepository.findAll()).willReturn(List.of(ce, ee));
+        given(majorRepository.findById(67L)).willReturn(Optional.of(ee));
         faculty = new Faculty("ece");
         faculty.addMajor(ce);
         this.addCourseService = new AddCourseService(courseRepository, majorRepository);
@@ -70,23 +69,19 @@ public class AddCourseServiceTest {
 
     @Test
     public void Unreal_major_is_not_returned_correctly() {
-        Long id1 = 67L;
+        Long id1 = 68L;
         String error = "";
         try {
             addCourseService.getMajors(Set.of(id1), faculty);
         } catch (ExceptionList exceptionList) {
             error = exceptionList.toString();
         }
-        assertEquals(error, "{\"1\":\"Major with id = 67 was not found.\"}");
+        assertEquals(error, "{\"1\":\"Major with id = 68 was not found.\"}");
     }
 
     @Test
     public void Major_that_is_not_in_faculty_is_not_returned_correctly() {
         Long id1 = 67L ;
-        for (Major m: majorRepository.findAll())
-            if (m.getMajorName().equals("EE"))
-                id1 = m.getId();
-
         String error = "";
         try {
             addCourseService.getMajors(Set.of(id1), faculty);
