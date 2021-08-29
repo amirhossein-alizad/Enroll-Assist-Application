24992a3618b1905a800995f71bcc840096344758
awwmiir <amir.alizad@ut.ac.ir>
2021-08-29 20:19:28 +0430

    Add majors list to FacultyView

--- src/main/java/ir/proprog/enrollassist/controller/FacultyView.java
+++ src/main/java/ir/proprog/enrollassist/controller/FacultyView.java
@@ -3,11 +3,15 @@ package ir.proprog.enrollassist.controller;
 import ir.proprog.enrollassist.domain.Faculty;
 import ir.proprog.enrollassist.domain.Major;
 import lombok.Getter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 public class FacultyView {
     private Long facultyId;
     private String facultyName;
+    private Set<Long> majors = new HashSet<>();
 
     public FacultyView() {
     }
@@ -15,5 +19,6 @@ public class FacultyView {
     public FacultyView(Faculty faculty) {
         this.facultyId = faculty.getId();
         this.facultyName = faculty.getFacultyName();
+        this.majors = faculty.getMajors().stream().map(Major::getId).collect(Collectors.toSet());
     }
 }
