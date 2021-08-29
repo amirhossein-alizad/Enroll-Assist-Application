cf3c421c4004df1efca192d18b4de8c74b822128
awwmiir <amir.alizad@ut.ac.ir>
2021-08-29 21:27:21 +0430

    Add majors list to controller

--- src/main/java/ir/proprog/enrollassist/controller/FacultyController.java
+++ src/main/java/ir/proprog/enrollassist/controller/FacultyController.java
@@ -2,13 +2,18 @@ package ir.proprog.enrollassist.controller;
 
 import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.domain.Faculty;
+import ir.proprog.enrollassist.domain.Major;
 import ir.proprog.enrollassist.repository.FacultyRepository;
+import ir.proprog.enrollassist.repository.MajorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -17,6 +22,7 @@ import java.util.stream.StreamSupport;
 @RequestMapping("/faculties")
 public class FacultyController {
     private final FacultyRepository facultyRepository;
+    private final MajorRepository majorRepository;
 
     @GetMapping
     public Iterable<FacultyView> all() {
@@ -26,6 +32,17 @@ public class FacultyController {
     @PostMapping( consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public FacultyView addOne(@RequestBody FacultyView facultyView) {
         ExceptionList exceptions = new ExceptionList();
+        Set<Major> majors = new HashSet<>();
+        for(Long id: facultyView.getMajors()){
+            Optional<Major> major = majorRepository.findById(id);
+            if(major.isEmpty()) {
+                exceptions.addNewException(new Exception("Major with id:" + id + " does not exist."));
+                continue;
+            }
+            majors.add(major.get());
+        }
+        if (exceptions.hasException())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exceptions.toString());
         if (facultyRepository.findByFacultyName(facultyView.getFacultyName()).isPresent())
             exceptions.addNewException(new Exception("Faculty with name " + facultyView.getFacultyName() + " exists."));
         Faculty faculty = null;
@@ -36,6 +53,8 @@ public class FacultyController {
         }
         if (exceptions.hasException())
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exceptions.toString());
+        for(Major m:majors)
+            faculty.addMajor(m);
         facultyRepository.save(faculty);
         return new FacultyView(faculty);
     }
