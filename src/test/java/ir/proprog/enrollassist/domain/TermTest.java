37f63544fa5151aec700e3526186e3e756dbc9cd
Sarina <sarinahamedani3@gmail.com>
2021-09-06 16:44:26 +0430

    Remove try-catch blocks in Term tests

--- src/test/java/ir/proprog/enrollassist/domain/TermTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/TermTest.java
@@ -1,55 +1,39 @@
 package ir.proprog.enrollassist.domain;
 
+import ir.proprog.enrollassist.Exception.ExceptionList;
+import ir.proprog.enrollassist.domain.major.Major;
+import ir.proprog.enrollassist.domain.student.Student;
 import ir.proprog.enrollassist.domain.studyRecord.Term;
+import ir.proprog.enrollassist.domain.utils.TestStudentBuilder;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class TermTest {
 
     @Test
     void Term_with_empty_field_cant_be_created() {
-        String error = "";
-        try {
-            Term term = new Term("");
-        } catch (Exception e) {
-            error = e.getMessage();
-        }
-        assertEquals(error, "Term can not be empty.");
+        Throwable error = assertThrows(Exception.class, () -> { Term term = new Term(""); });
+        assertEquals(error.getMessage(), "Term can not be empty.");
     }
 
     @Test
     void Term_with_none_number_string_cant_be_created() {
-        String error = "";
-        try {
-            Term term = new Term("AS23");
-        } catch (Exception e) {
-            error = e.getMessage();
-        }
-        assertEquals(error, "Term format  must be number.");
+        Throwable error = assertThrows(Exception.class, () -> { Term term = new Term("AS23"); });
+        assertEquals(error.getMessage(), "Term format  must be number.");
     }
 
     @Test
     void Term_with_length_that_is_not_equal_to_5_is_not_created() {
-        String error = "";
-        try {
-            Term term = new Term("1234");
-        } catch (Exception e) {
-            error = e.getMessage();
-        }
-        assertEquals(error, "Term format is not valid.");
+        Throwable error = assertThrows(Exception.class, () -> { Term term = new Term("1234"); });
+        assertEquals(error.getMessage(), "Term format is not valid.");
     }
 
     @Test
     void Term_with_invalid_season_is_not_created() {
-        String error = "";
-        try {
-            Term term = new Term("12985");
-        } catch (Exception e) {
-            error = e.getMessage();
-        }
-        assertEquals(error, "Season of term is not valid.");
+        Throwable error = assertThrows(Exception.class, () -> { Term term = new Term("12985"); });
+        assertEquals(error.getMessage(), "Season of term is not valid.");
     }
 
     @Test
