9df2ba85e2b1eab02aff374a5fdef932df43526d
Sarina <sarinahamedani3@gmail.com>
2021-09-06 16:49:21 +0430

    Rename test methods in Term tests

--- src/test/java/ir/proprog/enrollassist/domain/TermTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/TermTest.java
@@ -8,25 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TermTest {
 
     @Test
-    void Term_with_empty_field_cant_be_created() {
+    void Term_with_empty_field_cannot_be_created() {
         Throwable error = assertThrows(Exception.class, () -> { Term term = new Term(""); });
         assertEquals(error.getMessage(), "Term can not be empty.");
     }
 
     @Test
-    void Term_with_none_number_string_cant_be_created() {
+    void Term_number_can_only_consist_of_numerical_characters() {
         Throwable error = assertThrows(Exception.class, () -> { Term term = new Term("AS23"); });
         assertEquals(error.getMessage(), "Term format  must be number.");
     }
 
     @Test
-    void Term_with_length_that_is_not_equal_to_5_is_not_created() {
+    void Term_number_can_only_contain_five_digits() {
         Throwable error = assertThrows(Exception.class, () -> { Term term = new Term("1234"); });
         assertEquals(error.getMessage(), "Term format is not valid.");
     }
 
     @Test
-    void Term_with_invalid_season_is_not_created() {
+    void Term_with_invalid_season_cannot_be_created() {
         Throwable error = assertThrows(Exception.class, () -> { Term term = new Term("12985"); });
         assertEquals(error.getMessage(), "Season of term is not valid.");
     }
