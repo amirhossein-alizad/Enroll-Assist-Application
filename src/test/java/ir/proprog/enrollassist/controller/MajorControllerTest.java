83516a55847c0c2f741c2f1bddc297fc27d081b2
awwmiir <amir.alizad@ut.ac.ir>
2021-09-06 13:37:51 +0430

    Add setUp methods to MajorControllerTest

--- src/test/java/ir/proprog/enrollassist/controller/MajorControllerTest.java
+++ src/test/java/ir/proprog/enrollassist/controller/MajorControllerTest.java
@@ -1,9 +1,12 @@
 package ir.proprog.enrollassist.controller;
 
+import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.controller.major.MajorController;
 import ir.proprog.enrollassist.domain.course.Course;
 import ir.proprog.enrollassist.domain.faculty.Faculty;
 import ir.proprog.enrollassist.domain.major.Major;
+import ir.proprog.enrollassist.domain.utils.TestCourseBuilder;
+import ir.proprog.enrollassist.domain.utils.TestMajorBuilder;
 import ir.proprog.enrollassist.repository.*;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.CoreMatchers.anyOf;
 
 import java.util.List;
 
@@ -33,18 +37,50 @@ public class MajorControllerTest {
     private MajorRepository majorRepository;
     @MockBean
     private FacultyRepository facultyRepository;
+    private Major major1, major2;
+    private Course course1, course2;
+    private Faculty faculty;
+    JSONObject request;
+    JSONObject response;
 
+    void setUpWithTestBuilders() throws ExceptionList{
+        major1 = new TestMajorBuilder()
+                .majorName("CE")
+                .majorNumber("8101")
+                .build();
+        major2 = new TestMajorBuilder()
+                .majorName("CHEM")
+                .majorNumber("8102")
+                .build();
+        course1 = new TestCourseBuilder()
+                .courseNumber("1111111")
+                .title("C1")
+                .credits(3)
+                .graduateLevel("Undergraduate")
+                .build();
+        course2 = new TestCourseBuilder()
+                .courseNumber("2222222")
+                .title("C2")
+                .credits(4)
+                .graduateLevel("Undergraduate")
+                .build();
+    }
+
+    void setUpWithMock() {
+        faculty = mock(Faculty.class);
+        major1 = mock(Major.class);
+        request = new JSONObject();
+        response = new JSONObject();
+    }
     @Test
     public void All_majors_are_returned_correctly() throws Exception{
-        Major m1 = new Major("8101", "CS");
-        Major m2 = new Major("8102", "CHEM");
-        Major m3 = new Major("8103", "POLY");
-        given(majorRepository.findAll()).willReturn(List.of(m1, m2, m3));
+        setUpWithTestBuilders();
+        given(majorRepository.findAll()).willReturn(List.of(major1, major2));
         mvc.perform(get("/majors")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(3)))
-                .andExpect(jsonPath("$[0].majorName", is("CS")))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].majorName", is("CE")))
                 .andExpect(jsonPath("$[0].majorNumber", is("8101")))
                 .andExpect(jsonPath("$[1].majorName", is("CHEM")))
                 .andExpect(jsonPath("$[1].majorNumber", is("8102")));
@@ -52,24 +88,20 @@ public class MajorControllerTest {
 
     @Test
     public void Major_courses_is_returned_correctly() throws Exception{
-        Major major1 = new Major("8101", "CE");
-        Course c1 = new Course("1111111", "C1", 3, "Undergraduate");
-        Course c2 = new Course("1111112", "C2", 3, "Undergraduate");
-        Course c3 = new Course("1111113", "C3", 3, "Undergraduate");
-        major1.addCourse(c1, c2, c3);
+        setUpWithTestBuilders();
+        major1.addCourse(course1, course2);
         given(majorRepository.findById(1L)).willReturn(java.util.Optional.of(major1));
         mvc.perform(get("/majors/1/courses")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(3)))
-                .andExpect(jsonPath("$[0].courseTitle", is("C1")))
-                .andExpect(jsonPath("$[1].courseCredits", is(3)));
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].courseTitle", anyOf(is("C1"), is("C2"))))
+                .andExpect(jsonPath("$[1].courseCredits", anyOf(is(3), is(4))));
     }
 
     @Test
     public void Valid_major_is_added_correctly() throws Exception{
-        Faculty faculty = mock(Faculty.class);
-        JSONObject request = new JSONObject();
+        setUpWithMock();
         request.put("majorName", "CHEM");
         request.put("majorNumber", "8102");
         given(facultyRepository.findById(1L)).willReturn(java.util.Optional.ofNullable(faculty));
@@ -83,17 +115,14 @@ public class MajorControllerTest {
 
     @Test
     public void Major_cannot_be_added_if_name_already_exists() throws Exception{
-        Faculty faculty = mock(Faculty.class);
-        Major major = mock(Major.class);
-        JSONObject request = new JSONObject();
+        setUpWithMock();
         request.put("majorName", "EE");
         request.put("majorNumber", "8101");
 
-        JSONObject response = new JSONObject();
         response.put("1", "Major with name EE exists.");
 
         given(facultyRepository.findById(1L)).willReturn(java.util.Optional.ofNullable(faculty));
-        given(majorRepository.findByMajorName("EE")).willReturn(java.util.Optional.ofNullable(major));
+        given(majorRepository.findByMajorName("EE")).willReturn(java.util.Optional.ofNullable(major1));
         mvc.perform(post("/majors/1")
                 .content(request.toString())
                 .contentType(MediaType.APPLICATION_JSON))
@@ -103,11 +132,10 @@ public class MajorControllerTest {
 
     @Test
     public void Major_cannot_be_added_if_faculty_does_not_exists() throws Exception{
-        Major major = mock(Major.class);
-        JSONObject request = new JSONObject();
+        setUpWithMock();
         request.put("majorName", "EE");
         request.put("majorNumber", "8101");
-        given(majorRepository.findByMajorName("EE")).willReturn(java.util.Optional.ofNullable(major));
+        given(majorRepository.findByMajorName("EE")).willReturn(java.util.Optional.ofNullable(major1));
         mvc.perform(post("/majors/1")
                 .content(request.toString())
                 .contentType(MediaType.APPLICATION_JSON))
@@ -117,14 +145,11 @@ public class MajorControllerTest {
 
     @Test
     public void Major_name_and_number_cannot_be_empty() throws Exception{
-        Faculty faculty = mock(Faculty.class);
-        JSONObject request = new JSONObject();
+        setUpWithMock();
         request.put("majorName", "");
         request.put("majorNumber", "");
-
-        JSONObject response = new JSONObject();
-        response.put("1", "Major name can not be empty");
-        response.put("2", "Major number can not be empty");
+        response.put("1", "Major name can not be empty.");
+        response.put("2", "Major number can not be empty.");
 
         given(facultyRepository.findById(1L)).willReturn(java.util.Optional.ofNullable(faculty));
 
