913752aed78d30649321d00d967e1b03b530602b
awwmiir <amir.alizad@ut.ac.ir>
2021-09-06 07:09:19 +0430

    Use assertThrows instead of assertEquals

--- src/test/java/ir/proprog/enrollassist/domain/CourseTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/CourseTest.java
@@ -45,82 +45,68 @@ public class CourseTest {
 
     @Test
     void Course_number_and_course_title_can_not_be_empty() {
-        try {
-            Course math1 = new TestCourseBuilder()
-                    .courseNumber("")
-                    .title("")
-                    .credits(3)
-                    .graduateLevel("Undergraduate")
-                    .build();
-        } catch (ExceptionList e) {
-            errors.addExceptions(e.getExceptions());
-        }
-        assertEquals(errors.toString(), "{\"1\":\"Course must have a name.\"," +
+        Throwable error = assertThrows(ExceptionList.class,
+                () -> {
+                    new TestCourseBuilder()
+                            .courseNumber("")
+                            .title("")
+                            .credits(3)
+                            .graduateLevel("Undergraduate")
+                            .build();
+                });
+        assertEquals(error.toString(), "{\"1\":\"Course must have a name.\"," +
                 "\"2\":\"Course number cannot be empty.\"}");
     }
 
     @Test
     void Course_number_cannot_be_less_than_7_digits_and_credits_can_not_have_a_negative_value() {
-        try {
-            Course math1 = new TestCourseBuilder()
-                    .courseNumber("1")
-                    .title("MATH1")
-                    .credits(-3)
-                    .graduateLevel("Undergraduate")
-                    .build();
-        } catch (ExceptionList e) {
-            errors.addExceptions(e.getExceptions());
-        }
-        assertEquals(errors.toString(),
+        Throwable error = assertThrows(ExceptionList.class,
+                () -> {
+                    new TestCourseBuilder()
+                            .courseNumber("1")
+                            .title("MATH1")
+                            .credits(-3)
+                            .graduateLevel("Undergraduate")
+                            .build();
+                });
+        assertEquals(error.toString(),
                 "{\"1\":\"Course number must contain 7 numbers.\","
                         + "\"2\":\"Credit must be one of the following values: 0, 1, 2, 3, 4.\"}");
     }
 
     @Test
-    void Course_with_no_prerequisites_can_be_taken_by_anybody() {
+    void Course_with_no_prerequisites_can_be_taken_by_anybody() throws ExceptionList{
         when(bebe.hasPassed(any(Course.class))).thenReturn(false);
-        Course math1 = null;
-        try {
-            math1 = new TestCourseBuilder()
-                    .courseNumber("1111111")
-                    .title("MATH1")
-                    .credits(3)
-                    .graduateLevel("Undergraduate")
-                    .build();
-        } catch (ExceptionList e) {
-            errors.addExceptions(e.getExceptions());
-        }
-        assertFalse(errors.hasException());
+        Course math1 = new TestCourseBuilder()
+                .courseNumber("1111111")
+                .title("MATH1")
+                .credits(3)
+                .graduateLevel("Undergraduate")
+                .build();
         assertThat(math1.canBeTakenBy(bebe))
                 .isNotNull()
                 .isEmpty();
     }
 
     @Test
-    void Course_with_one_prerequisite_can_be_taken_by_who_has_passed_pre() {
+    void Course_with_one_prerequisite_can_be_taken_by_who_has_passed_pre() throws ExceptionList{
         when(bebe.hasPassed(any(Course.class))).thenReturn(true);
-        Course math1;
-        try {
-            math1 = new TestCourseBuilder()
-                    .courseNumber("1111111")
-                    .title("MATH1")
-                    .credits(3)
-                    .graduateLevel("Undergraduate")
-                    .build();
-            Course math2 = new TestCourseBuilder()
-                    .courseNumber("2222222")
-                    .title("MATH2")
-                    .credits(3)
-                    .graduateLevel("Undergraduate")
-                    .build()
-                    .withPre(math1);
-            assertThat(math2.canBeTakenBy(bebe))
-                    .isNotNull()
-                    .isEmpty();
-        } catch (ExceptionList e) {
-            errors.addExceptions(e.getExceptions());
-        }
-        assertFalse(errors.hasException());
+        Course math1 = new TestCourseBuilder()
+                .courseNumber("1111111")
+                .title("MATH1")
+                .credits(3)
+                .graduateLevel("Undergraduate")
+                .build();
+        Course math2 = new TestCourseBuilder()
+                .courseNumber("2222222")
+                .title("MATH2")
+                .credits(3)
+                .graduateLevel("Undergraduate")
+                .build()
+                .withPre(math1);
+        assertThat(math2.canBeTakenBy(bebe))
+                .isNotNull()
+                .isEmpty();
     }
 
     @Test
