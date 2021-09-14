69dc70e3fe508b3af56a071a709f7fdf8f137f25
SarinaHamedani <sarinahamedani3@gmail.com>
2021-09-14 11:37:43 +0430

    Add API for adding new user

--- src/main/java/ir/proprog/enrollassist/controller/user/UserController.java
+++ src/main/java/ir/proprog/enrollassist/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package ir.proprog.enrollassist.controller.user;
 
+import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.controller.student.StudentView;
 import ir.proprog.enrollassist.domain.student.Student;
 import ir.proprog.enrollassist.domain.student.StudentNumber;
@@ -28,6 +29,23 @@ public class UserController {
     public UserView one(@PathVariable String id) {
         User user = this.userRepository.findByUserId(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        return new UserView(user);
+    }
+
+    @PostMapping
+    public UserView addOne(@RequestBody UserView userView) {
+        ExceptionList exceptionList = new ExceptionList();
+        User user = null;
+        try {
+            user = new User(userView.getName(), userView.getUserId());
+        } catch (IllegalArgumentException ex) {
+            exceptionList.addNewException(ex);
+        }
+        if (this.userRepository.findByUserId(userView.getUserId()).isPresent())
+            exceptionList.addNewException(new Exception("User Id already exists."));
+
+        if (exceptionList.hasException())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exceptionList.toString());
         return new UserView(user);
     }
 }
