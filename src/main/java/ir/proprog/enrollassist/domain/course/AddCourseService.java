45fff58dfc31f31be8d12b8e912622794049df9f
SarinaHamedani <sarinahamedani3@gmail.com>
2021-09-14 20:15:07 +0430

    Refactor addCourse method in AddCourseService

--- src/main/java/ir/proprog/enrollassist/domain/course/AddCourseService.java
+++ src/main/java/ir/proprog/enrollassist/domain/course/AddCourseService.java
@@ -2,19 +2,13 @@ package ir.proprog.enrollassist.domain.course;
 
 import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.controller.course.CourseMajorView;
-import ir.proprog.enrollassist.domain.faculty.Faculty;
-import ir.proprog.enrollassist.domain.major.Major;
 import ir.proprog.enrollassist.domain.program.Program;
 import ir.proprog.enrollassist.repository.CourseRepository;
-import ir.proprog.enrollassist.repository.FacultyRepository;
-import ir.proprog.enrollassist.repository.MajorRepository;
 import ir.proprog.enrollassist.repository.ProgramRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Service
 @AllArgsConstructor
@@ -29,15 +23,10 @@ public class AddCourseService {
             exceptionList.addNewException(new Exception("Course number already exists."));
 
         Course course = null;
-        Set<Course> prerequisites = new HashSet<>();
-        Set<Program> programs = new HashSet<>();
+        Set<Program> programs = getPrograms(input.getPrograms(), exceptionList);
+        this.validatePrerequisites(input.getPrerequisites(), exceptionList);
+        Set<Course> prerequisites = this.getPrerequisites(input.getPrerequisites());
 
-        try {
-            programs = getPrograms(input.getPrograms());
-        } catch (ExceptionList e) { exceptionList.addExceptions(e.getExceptions()); }
-        try {
-            prerequisites = this.validatePrerequisites(input.getPrerequisites());
-        } catch (ExceptionList e) { exceptionList.addExceptions(e.getExceptions()); }
         try {
             course = new Course(input.getCourseNumber().getCourseNumber(), input.getCourseTitle(), input.getCourseCredits(), input.getGraduateLevel().toString());
             course.setPrerequisites(prerequisites);
@@ -46,30 +35,35 @@ public class AddCourseService {
         if (exceptionList.hasException())
             throw exceptionList;
 
-        for(Program p:programs) {
-            try {
-                p.addCourse(course);
-                this.programRepository.save(p);
-            } catch (ExceptionList ex) {
-                exceptionList.addExceptions(ex.getExceptions());
-            }
-        }
+        Course finalCourse = course;
+        programs.forEach(p -> addCourseToProgram(p, finalCourse, exceptionList));
         if (exceptionList.hasException())
             throw exceptionList;
 
+        this.programRepository.saveAll(programs);
         this.courseRepository.save(course);
         return course;
     }
 
-    private Set<Course> validatePrerequisites(Set<Long> prerequisiteIds) throws ExceptionList {
-        ExceptionList exceptionList = new ExceptionList();
+    private void addCourseToProgram(Program program, Course course, ExceptionList exceptionList) {
+        try {
+            program.addCourse(course);
+        } catch (ExceptionList ex) {
+            exceptionList.addExceptions(ex.getExceptions());
+        }
+    }
+
+    private Set<Course> getPrerequisites(Set<Long> prerequisiteIds) {
         Set<Course> prerequisites = new HashSet<>();
-        prerequisiteIds.forEach(L -> courseRepository.findById(L).
-                ifPresentOrElse(c -> checkLoop(c, exceptionList), () -> exceptionList.addNewException(new Exception(String.format("Course with id = %s was not found.", L)))));
-        if (exceptionList.hasException())
-            throw exceptionList;
         courseRepository.findAllById(prerequisiteIds).forEach(prerequisites::add);
         return prerequisites;
+    }
+
+    private void validatePrerequisites(Set<Long> prerequisiteIds, ExceptionList exceptionList) throws ExceptionList {
+        ExceptionList exceptions = new ExceptionList();
+        prerequisiteIds.forEach(L -> courseRepository.findById(L).
+                ifPresentOrElse(c -> checkLoop(c, exceptions), () -> exceptions.addNewException(new Exception(String.format("Course with id = %s was not found.", L)))));
+        exceptionList.addExceptions(exceptions.getExceptions());
     }
 
     private void checkLoop(Course prerequisite, ExceptionList exceptions) {
@@ -87,14 +81,13 @@ public class AddCourseService {
         }
     }
 
-    public Set<Program> getPrograms(Set<Long> ids) throws ExceptionList {
-        ExceptionList exceptionList = new ExceptionList();
+    public Set<Program> getPrograms(Set<Long> ids, ExceptionList exceptions) throws ExceptionList {
         Set<Program> programs = new HashSet<>();
+        ExceptionList exceptionList = new ExceptionList();
         ids.forEach(L -> programRepository.findById(L).
                 ifPresentOrElse(programs::add,
                         () -> exceptionList.addNewException(new Exception(String.format("Program with id = %s was not found.", L)))));
-        if (exceptionList.hasException())
-            throw exceptionList;
+        exceptions.addExceptions(exceptionList.getExceptions());
         return programs;
     }
 }
